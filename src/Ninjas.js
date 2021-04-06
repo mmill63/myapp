c6b8faaa67b7a3f5e97dbfbc2b8e30b3f6241f3a
MichealMill <mmill63@gmail.com>
2021-04-06 15:30:47 +0300

    stateless components

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 
 
-class Ninjas extends Component{
-    render(){
-        //console.log(this.props);
-        const {ninjas} = this.props;
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className="ninja" key={ninja.id}>
-                    <div>Name: {ninja.name}</div>
-                    <div>Age: {ninja.age}</div>
-                </div>
-            )
-        })
-        return(
-            <div className="ninja-list">
-                {ninjaList}
+const Ninjas = ({ninjas}) => {    
+    const ninjaList = ninjas.map(ninja =>{
+        return ninja.age > 25 ? (
+            <div className="ninja" key={ninja.id}>
+                <div>Name: {ninja.name}</div>
+                <div>Age: {ninja.age}</div>
             </div>
-        )
-    }
+        ) : null
+    })
+    return(
+        <div className="ninja-list">
+            {ninjaList}
+        </div>
+    )   
 }
 export default Ninjas
