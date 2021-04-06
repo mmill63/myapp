badd796fe5cde7719fb46e2cc930825347c3807a
MichealMill <mmill63@gmail.com>
2021-04-06 13:31:57 +0300

    created Ninja.js component

---
+++ src/Ninjas.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function Ninjas(){
+    return(
+        <div className="ninja">
+          <div>Name:</div>
+          <div>Age:</div>
+          <div>Belt:</div>  
+        </div>
+    );
+}
+
+export default Ninjas
