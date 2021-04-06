2929a2fb549ec5b4eac314bf3e8769793774f051
MichealMill <mmill63@gmail.com>
2021-04-06 18:12:07 +0300

    add and delete

--- src/App.js
+++ src/App.js
@@ -10,13 +10,28 @@ class App extends Component {
       {name : 'Dor', age: 76, id: 3}
     ]
   }
+  addNinja = (ninja) =>{
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  deleteNinja = (id) =>{
+    let ninjas = this.state.ninjas.filter(ninja =>{
+      return ninja.id !== id  
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>React APP</h1>
         <p>Hallo</p>
-        <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja />
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 
-const Ninjas = ({ninjas}) => {    
+const Ninjas = ({ninjas, deleteNinja}) => {    
     const ninjaList = ninjas.map(ninja =>{
         return ninja.age > 25 ? (
             <div className="ninja" key={ninja.id}>
                 <div>Name: {ninja.name}</div>
                 <div>Age: {ninja.age}</div>
+                <button onClick={()=>{deleteNinja(ninja.id)}}>Delete</button>
             </div>
         ) : null
     })
